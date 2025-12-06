9011d326f344fec61d495b81a141170e9b2edfec
Yavuz Beydogan <y.beydogan33@gmail.com>
2025-12-06 16:20:27 +0300

    4. Uygulama

--- consoleApp4/Program.cs
+++ consoleApp4/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿int a = 10;
+int b = 5;
+int c = 20;
+var sonuc = (c-a)*b;
+Console.WriteLine(sonuc);
+
+int? a1 = null ;
+int b1 = 20;
+var sonuc1 = (a1 ?? 0)+b1;
+Console.WriteLine(sonuc1);
+
+int a2 = 10;
+int b2 = 20;
+a2=b2--;
+Console.WriteLine(a2);
+Console.WriteLine(b2);
+
+Console.Write("Sayı:");
+int sayi = int.Parse(Console.ReadLine() ?? "0");
+int sonuc2 = sayi % 2; 
+if (sonuc2 == 1){
+    Console.WriteLine("Tek");
+}
+else{
+    Console.WriteLine("Çift");
+}
+
+
+
+
+
