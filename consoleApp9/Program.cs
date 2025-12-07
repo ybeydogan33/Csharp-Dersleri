a505ca9ca1714765cc08078b6f7626445667a31d
Yavuz Beydogan <y.beydogan33@gmail.com>
2025-12-07 19:09:43 +0300

    9. Uygulama

---
+++ consoleApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿var rnd = new Random();
+var tutulan = rnd.Next(1,100);
+Console.WriteLine($"Tutulan sayı: {tutulan}");
+for (var hak =5; hak>0; hak--){
+    Console.Write($"{hak}. hak: ");
+    int tahmin = int.Parse(Console.ReadLine()??"0");
+    if(tahmin <tutulan){
+        Console.WriteLine($"daha yukarı tahmin et.");
+        continue;
+    }
+    else if (tahmin > tutulan){
+        Console.WriteLine($"daha aşağı tahmin et.");
+        continue;
+    }
+    else if (hak ==0){
+        Console.WriteLine("Hakkınız bitti kaybettiniz.");
+        break;
+    }
+    else{
+        Console.WriteLine("Tebrikler doğru bildiniz.");
+        break;
+    }
+}
