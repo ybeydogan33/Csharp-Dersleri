544b50f98f3ad46ebe2ecb2b9b7530bfe8c52b6e
Yavuz Beydogan <y.beydogan33@gmail.com>
2025-12-16 17:17:05 +0300

    14. Uygulama

---
+++ consoleApp14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace consoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var yazdir = HelperMethod.karakterDuzelt("ölçme ve değerlendirme");
+            Console.WriteLine(yazdir);
+        }
+    }
+    class HelperMethod
+    {
+        public static string karakterDuzelt(string str)
+        {
+            return str.Replace("ö","o")
+            .Replace("ü","u")
+            .Replace("ı","i")
+            .Replace("ğ","g")
+            .Replace("ç","c")
+            .Replace(" ","-");
+        }
+    }
+}
