04cceea4e4d7b83255f3a9d26d63cbf65bb93518
Yavuz Beydogan <y.beydogan33@gmail.com>
2025-12-16 11:07:44 +0300

    10. Uygulama

--- consoleApp10/Program.cs
+++ consoleApp10/Program.cs
@@ -1,40 +1,40 @@
 ﻿using System;
-namespace consoleApp10
+namespace ConsoleApp10
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
             Ogrenci ogr1 = new Ogrenci();
-            ogr1.Ad = "Yavuz";
-            ogr1.No = "701";
-            ogr1.Sube = "7/E";
+            ogr1.ad = "Yavuz";
+            ogr1.no = "701";
+            ogr1.sube = "7/E";
 
             Ogrenci ogr2 = new Ogrenci();
-            ogr2.Ad = "Sevgi";
-            ogr2.No = "123";
-            ogr2.Sube = "7/A";
+            ogr2.ad = "Sevgi";
+            ogr2.no = "123";
+            ogr2.sube = "7/E";
 
             Ogrenci ogr3 = new Ogrenci();
-            ogr3.Ad = "Yiğit";
-            ogr3.No = "456";
-            ogr3.Sube = "5/A";
+            ogr3.ad = "Yigit";
+            ogr3.no = "456";
+            ogr3.sube = "5/A";
 
             Ogrenci[] ogrenciler = new Ogrenci[3];
-            ogrenciler[0]= ogr1;
-            ogrenciler[1]= ogr2;
-            ogrenciler[2]= ogr3;
+            ogrenciler[0]=ogr1;
+            ogrenciler[1]=ogr2;
+            ogrenciler[2]=ogr3;
 
-            foreach(var ogrenci in ogrenciler)
+            foreach (var ogrenci in ogrenciler)
             {
-                Console.WriteLine($"{ogrenci.Ad} isimli öğrencinin şubesi {ogrenci.Sube} ve numarası {ogrenci.No}");
+                Console.WriteLine($"{ogrenci.ad} isimli öğrencinin şubesi:{ogrenci.sube} numarası{ogrenci.no}");
             }
-        }
+        } 
     }
     class Ogrenci
     {
-        public string Ad { get; set;}
-        public string No { get; set;}
-        public string Sube { get; set;}
+        public string ad {get; set;}
+        public string no {get; set;}
+        public string sube {get; set;}
     }
 }
